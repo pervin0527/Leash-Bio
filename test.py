a5cbe242e313653cd06ba09406345d08b599ada5
pervin0527 <zfbtldnz@gmail.com>
2024-06-15 22:16:02 +0900

    Updated 2024.06.15

--- test.py
+++ test.py
@@ -11,8 +11,8 @@ import matplotlib.pyplot as plt
 from data.dataset import process_smiles_list, preprocess_data
 from util.utils import load_config, load_models, load_top_k_models, predict_with_models
 
-def plot_feature_importance(weights_dir, k, n):
-    utils_dir = os.path.join(weights_dir, 'utils')
+def plot_feature_importance(cfg):
+    utils_dir = os.path.join(cfg['ckpt_dir'], 'utils')
     performance_file = os.path.join(utils_dir, 'model_performance.json')
     models = load_top_k_models(performance_file, k)
 
@@ -29,7 +29,7 @@ def plot_feature_importance(weights_dir, k, n):
     avg_feature_importance = {name: np.mean(scores) for name, scores in feature_importance_dict.items()}    
     sorted_features = sorted(avg_feature_importance.items(), key=lambda x: x[1], reverse=True)
 
-    top_n_features = sorted_features[:n]
+    top_n_features = sorted_features[:cfg['feature_top_n']]
     feature_names, importance_scores = zip(*top_n_features)
 
     grouped_feature_importance = {}
@@ -44,35 +44,31 @@ def plot_feature_importance(weights_dir, k, n):
             grouped_feature_importance[group_name] = score
 
     sorted_grouped_features = sorted(grouped_feature_importance.items(), key=lambda x: x[1], reverse=True)
-    grouped_feature_names, grouped_importance_scores = zip(*sorted_grouped_features[:n])
+    grouped_feature_names, grouped_importance_scores = zip(*sorted_grouped_features[:cfg['feature_top_n']])
 
     plt.figure(figsize=(10, 8))
     plt.barh(grouped_feature_names, grouped_importance_scores)
     plt.xlabel('Average Gain')
     plt.ylabel('Feature Group')
-    plt.title(f'Top {n} Feature Groups Importance from Top {k} Models')
+    plt.title(f'Top {cfg['feature_top_n']} Feature Groups Importance from Top {cfg['predict_top_k']} Models')
     plt.gca().invert_yaxis()
     plt.tight_layout()
 
-    os.makedirs(f"{weights_dir}/importance_scores", exist_ok=True)
-    importance_plot_path = os.path.join(weights_dir, 'importance_scores', 'feature_importance_grouped.png')
+    os.makedirs(f"{cfg['ckpt_dir']}/importance_scores", exist_ok=True)
+    importance_plot_path = os.path.join(cfg['ckpt_dir'], 'importance_scores', 'feature_importance_grouped.png')
     plt.savefig(importance_plot_path)
     plt.close()
     print(f"Grouped feature importance plot saved to {importance_plot_path}")
 
 
-def predict(test_parquet_path, saved_dir, radius, dim, k=5, chunk_size=10000):
+def predict(cfg):
     offset = 0
     conn = duckdb.connect()
-    total_rows_query = f"SELECT COUNT(*) FROM '{test_parquet_path}'"
+    total_rows_query = f"SELECT COUNT(*) FROM '{cfg['test_parquet']}'"
     total_rows = conn.execute(total_rows_query).fetchone()[0]
 
-    performance_file = os.path.join(f"{saved_dir}/utils", 'model_performance.json')
-    models = load_top_k_models(performance_file, k)
-
-    # Load the descriptor normalizer
-    normalizer_path = os.path.join(saved_dir, 'utils', 'descriptor_normalizer.pkl')
-    preprocessor = joblib.load(normalizer_path)
+    performance_file = os.path.join(f"{cfg['data_dir']}/utils", 'model_performance.json')
+    models = load_top_k_models(performance_file, cfg['predict_top_k'])
 
     result_file = './my_submission.csv'
     with open(result_file, 'w') as f:
@@ -81,8 +77,8 @@ def predict(test_parquet_path, saved_dir, radius, dim, k=5, chunk_size=10000):
     while offset < total_rows:
         query = f"""
         SELECT id, molecule_smiles, protein_name
-        FROM '{test_parquet_path}'
-        LIMIT {chunk_size} 
+        FROM '{cfg['test_parquet']}'
+        LIMIT {cfg['limit']} 
         OFFSET {offset}
         """
         chunk_df = conn.execute(query).fetchdf()
@@ -91,10 +87,10 @@ def predict(test_parquet_path, saved_dir, radius, dim, k=5, chunk_size=10000):
         smiles_list = chunk_df['molecule_smiles'].tolist()
         chunk_df = preprocess_data(chunk_df,
                                    smiles_list, 
-                                   f"{saved_dir}/utils/normalized_ctd.parquet", 
-                                   saved_dir, 
-                                   radius, 
-                                   dim, 
+                                   f"{cfg['data_dir']}/utils/normalized_ctd.parquet", 
+                                   cfg['data_dir'], 
+                                   cfg['radius'], 
+                                   cfg['vec_dim'], 
                                    is_train=False,
                                    important_features=['SMR_VSA4', 'SlogP_VSA1', 'fr_phenol', 'NumSaturatedCarbocycles', 'fr_Ar_NH'])
 
@@ -107,7 +103,7 @@ def predict(test_parquet_path, saved_dir, radius, dim, k=5, chunk_size=10000):
         })
 
         chunk_results.to_csv(result_file, mode='a', header=False, index=False)        
-        offset += chunk_size
+        offset += cfg['limit']
         print()
 
     conn.close()
